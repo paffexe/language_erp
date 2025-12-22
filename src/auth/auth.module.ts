e2903c6e8050e8b17cb788793a133a88c2579f59
paffexe <kamolov607@gmail.com>
2025-12-22 11:17:48 +0500

    newest update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
-import { AdminModule } from '../admin/admin.module';
-import { TeacherModule } from '../teacher/teacher.module';
 import { AccessTokenStrategy } from '../common/strategies/access-token-strategy';
 import { RefreshTokenCookieStrategy } from '../common/strategies/refresh-token-strategy';
 
